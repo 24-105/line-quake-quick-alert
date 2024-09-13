525a13d6c2f6d41d00a8bcc69dafedb1079405df
tsujiryuma <r.0918.ryuma@icloud.com>
2024-09-14 00:36:31 +0900

    feat: インターフェイス追加

--- src/modules/lineModule.ts
+++ src/modules/lineModule.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { LineAuthController } from 'src/presentation/controllers/lineAuthController';
 import { AccessTokenService } from 'src/application/services/accessTokenService';
-import { LineApiService } from 'src/infrastructure/api/line/lineApiService';
+import { LineMessagingApiService } from 'src/infrastructure/api/line/lineMessagingApiService';
 
 @Module({
   imports: [HttpModule],
   controllers: [LineAuthController],
-  providers: [AccessTokenService, LineApiService],
+  providers: [AccessTokenService, LineMessagingApiService],
 })
 export class LineModule {}
