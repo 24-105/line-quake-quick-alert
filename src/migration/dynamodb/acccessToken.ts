29af795f693eb9356d0291c096fdeea7360d32a4
tsujiryuma <r.0918.ryuma@icloud.com>
2024-09-14 17:17:50 +0900

    feat: ローカルでのdynamodb設定

---
+++ src/migration/dynamodb/acccessToken.ts
@@ -0,0 +1,71 @@
+import {
+  CreateTableCommand,
+  DynamoDBClient,
+  UpdateTimeToLiveCommand,
+  ScalarAttributeType,
+  KeyType,
+} from '@aws-sdk/client-dynamodb';
+
+// 環境変数からクレデンシャルを取得
+const credentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'dummyAccessKeyId',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'dummySecretAccessKey',
+};
+
+// DynamoDBクライアントを作成する
+const client = new DynamoDBClient({
+  region: 'ap-northeast-1',
+  endpoint: 'http://localhost:8000',
+  credentials: credentials,
+});
+
+// アクセストークンテーブルを作成する
+const createTable = async () => {
+  const params = {
+    AttributeDefinitions: [
+      { AttributeName: 'channelID', AttributeType: ScalarAttributeType.S },
+      ,
+    ],
+    TableName: 'AccessToken',
+    KeySchema: [{ AttributeName: 'channelID', KeyType: KeyType.HASH }],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 1,
+      WriteCapacityUnits: 1,
+    },
+  };
+
+  try {
+    const data = await client.send(new CreateTableCommand(params));
+    console.log('AccessToken Table is successfully created', data);
+  } catch (err) {
+    console.error('AccessToken Table is failed to create', err);
+    throw err;
+  }
+};
+
+// TTLを有効化する
+const enableTTL = async () => {
+  const params = {
+    TableName: 'AccessToken',
+    TimeToLiveSpecification: {
+      AttributeName: 'TTL',
+      Enabled: true,
+    },
+  };
+
+  try {
+    const data = await client.send(new UpdateTimeToLiveCommand(params));
+    console.log('AccessToken Table is successfully updated', data);
+  } catch (err) {
+    console.error('AccessToken Table is failed to update', err);
+    throw err;
+  }
+};
+
+// テーブル作成後にTTLを有効化する
+const setupAccessTokenTable = async () => {
+  await createTable();
+  await enableTTL();
+};
+
+setupAccessTokenTable();
