0b6cd94cf999af886b4b434e77af23706a737e07
tsujiryuma <r.0918.ryuma@icloud.com>
2024-09-23 13:49:54 +0900

    docs: response terms and policy html

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
 
 /**
  * NestJSアプリケーションの起動
  */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(3000);
 }
 bootstrap();
