eca1da239f4f23fed2d6159580f8d082ac87d49f
tsujiryuma <r.0918.ryuma@icloud.com>
2024-10-06 17:09:11 +0900

    chore: rename directory

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
-import { HttpExceptionFilter } from './presentation/filter/httpExceptionFilter';
+import { HttpExceptionFilter } from './presentation/filters/httpExceptionFilter';
 
 /**
  * NestJSアプリケーションの起動
