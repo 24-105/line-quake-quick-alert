98dadd5babe4337fde29634d6215cb8dc7015308
tsujiryuma <r.0918.ryuma@icloud.com>
2024-10-06 16:24:11 +0900

    feat: http exception handling

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
+import { HttpExceptionFilter } from './presentation/filter/httpExceptionFilter';
 
 /**
  * NestJSアプリケーションの起動
  */
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(3000);
 }
